a8d7291f24f2268ee60c9ee864f73555ff0d9b80
kawasaki-atsushi <kawasaki8910@gmail.com>
2017-02-08 20:43:04 +0900

    fix

--- options.js
+++ options.js
@@ -1,29 +1,31 @@
 (function(win, doc){
+  var base64;
   var reader = new FileReader();
   var submit = doc.querySelector(".js-submit");
 
-  console.log(submit);
   var image = doc.querySelector(".js-image-input");
   var checkbox = doc.querySelector(".js-check-input");
-  console.log(checkbox);
-  console.log(image);
   submit.addEventListener('click', function(){
     if(image.files.length > 0) {
       base64 = reader.readAsDataURL(image.files[0]);
     }
 
-    var base64;
     reader.onload = function() {
-      if(reader) {
-        base64 = reader.result;
-      }
-      chrome.storage.local.set({
-        "image": base64 || null,
-        "enable": checkbox.checked
+      setChrome({
+        'image': reader.result,
+        'enable': checkbox.checked
       });
-      console.log(reader.result);
-      console.log(checkbox.checked);
-      console.log(chrome.storage);
+      return reader.result;
     };
+
+    setChrome({
+      'image': reader.result,
+      'enable': checkbox.checked
+    });
   });
+
+  function setChrome(data) {
+    var result = data || {};
+    chrome.storage.local.set(data);
+  }
 })(window, document);
